a60ab0a234fef6cbc958a558c103802349f5368e
Rahiful Islam <rahiful@uxd.co.jp>
2023-03-13 16:22:47 +0600

    1st commit

---
+++ appoinment-loan-calculator/src/App.js
@@ -0,0 +1,24 @@
+import { Grid } from "@mui/material";
+import Navbar from "./components/Navbar";
+import SliderSelect from "./components/SliderSelect";
+import Tenure from "./components/Tenure";
+import Result from "./components/Result";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Grid container spacing={5}>
+        <Grid item>
+          <SliderSelect/>
+          <Tenure/>
+        </Grid>
+        <Grid item>
+          <Result/>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
