abdc9e488b14642f80e7ec23f5df46e0700e19fc
Rahiful <rahifulislam@gmail.com>
2023-03-28 14:36:17 +0600

    update data using useState hook

--- appoinment-loan-calculator/src/components/SliderSelect.js
+++ appoinment-loan-calculator/src/components/SliderSelect.js
@@ -2,10 +2,53 @@
 import React from 'react'
 import SliderComponent from './common/SliderComponent'
 
-const SliderSelect = () => {
+const SliderSelect = ({data}) => {
   return (
     <div>
-      <SliderComponent amount={3500} unit="$" defaultValue ={3000} min={1000} max={10000} label={"Hello everyone"}/>
+      <SliderComponent
+        amount={data.homeValue}
+        unit="$"
+        defaultValue={data.homeValue}
+        value={data.homeValue}
+        min={1000}
+        max={10000}
+        step={100}
+        label={"Home value"}
+        onChange={(e) => console.log(e.target.value)}
+      />
+
+      <SliderComponent
+        amount={3500}
+        unit="$"
+        defaultValue={3000}
+        min={1000}
+        max={10000}
+        step={100}
+        label={"Down payment"}
+        onChange={(e) => console.log(e.target.value)}
+      />
+
+      <SliderComponent
+        amount={3500}
+        unit="$"
+        defaultValue={3000}
+        min={1000}
+        max={10000}
+        step={100}
+        label={"Loan amount"}
+        onChange={(e) => console.log(e.target.value)}
+      />
+
+      <SliderComponent
+        amount={2}
+        unit="%"
+        defaultValue={2}
+        min={2}
+        max={18}
+        step={.5}
+        label={"Interest rate"}
+        onChange={(e) => console.log(e.target.value)}
+      />
     </div>
   )
 }
--- appoinment-loan-calculator/src/components/common/SliderComponent.js
+++ appoinment-loan-calculator/src/components/common/SliderComponent.js
@@ -3,24 +3,29 @@ import Slider from '@mui/material/Slider';
 import { Stack, Typography } from '@mui/material';
 
 
-const SliderComponent = ({defaultValue, min, max, amount, label, unit}) => {
+const SliderComponent = ({ defaultValue, min, max, amount, label, unit, step, value, onChange }) => {
   return (
-    <>
-    <Stack>
-    <Stack>
-
+    <Stack my={1}>
+      <Stack gap={2}>
+        <Typography variant='subtitle2'>{label}</Typography>
+        <Typography variant='h5'>{unit} {amount}</Typography>
+      </Stack>
+      <Slider
+        defaultValue={defaultValue}
+        min={min}
+        max={max}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        value={value}
+        marks
+        step={step}
+        onChange= {onChange}
+      />
+      <Stack direction='row' justifyContent='space-between'>
+        <Typography variant='caption' color='text.secondary'>{unit} {min}</Typography>
+        <Typography variant='caption' color='text.secondary'>{unit} {max}</Typography>
+      </Stack>
     </Stack>
-    <Slider 
-    defaultValue={defaultValue} 
-    min={min} 
-    max={max} 
-    aria-label="Default" 
-    valueLabelDisplay="auto" 
-    />
-    </Stack>
-    
-    <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
-    </>
   );
 }
 
