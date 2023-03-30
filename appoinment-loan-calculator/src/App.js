234062ca950b26996738094e40604eb1d5b1b55b
Rahiful <rahifulislam@gmail.com>
2023-03-30 14:29:32 +0600

    last commit

--- appoinment-loan-calculator/src/App.js
+++ appoinment-loan-calculator/src/App.js
@@ -10,21 +10,21 @@ function App() {
     homeValue: 3000,
     downPayment: 3000 * 0.2,
     loanAmount: 3000 * 0.8,
-    loanTern: 5,
+    loanTerm: 5,
     interestRate: 5
   })
 
   return (
     <div className="App">
       <Navbar />
-      <Container maxWidth={"xl"}>
-        <Grid container spacing={5}>
-          <Grid item md={6}>
+      <Container maxWidth={"xl"} sx={{marginTop: 4}}>
+        <Grid container spacing={5} alignItems='center'>
+          <Grid item xs={12} md={6}>
             <SliderSelect data={data} setData={setData}/>
-            <Tenure />
+            <Tenure data={data} setData={setData}/>
           </Grid>
-          <Grid item md={6}>
-            <Result />
+          <Grid item xs={12} md={6}>
+            <Result data={data}/>
           </Grid>
         </Grid>
       </Container>
